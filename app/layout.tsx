85bf0883c51d98bf157d4df0a2ce6bbef5e52f32
shawn-goh24 <ahshawngoh@gmail.com>
2026-02-10 13:09:04 +0800

    Edit metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Alpeh Technology",
+  title: "AlpehTech Take home assignment",
   description: "Take home assignment",
 };
 
